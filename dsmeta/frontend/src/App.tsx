a7890ec9ab05607dd8b8ab63cdcdea23766e3c0e
natanael nogueira <natanaelnogueira019@gmail.com>
2022-11-15 20:43:40 -0300

    first component

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return (
+    <>
   <h1>Ola mundo</h1>
+  <NotificationButton/>
+  <NotificationButton/>
+  <NotificationButton/>
+  <NotificationButton/>
+  <NotificationButton/>
+  
+    
+
+  </>
   )
   }
 
