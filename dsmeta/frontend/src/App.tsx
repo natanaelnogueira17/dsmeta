92312100918b0f43d02c9d4e770dbfd9941732f1
natanael nogueira <natanaelnogueira019@gmail.com>
2022-11-16 00:07:43 -0300

    other components

--- dsmeta/frontend/src/App.tsx
+++ dsmeta/frontend/src/App.tsx
@@ -1,20 +1,23 @@
 import NotificationButton from "./components/NotificationButton"
-
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-  <h1>Ola mundo</h1>
-  <NotificationButton/>
-  <NotificationButton/>
-  <NotificationButton/>
-  <NotificationButton/>
-  <NotificationButton/>
-  
-    
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
 
-  </>
+
+
+            </div >
+        </section>
+      </main>
+    </>
   )
-  }
+}
 
 export default App
